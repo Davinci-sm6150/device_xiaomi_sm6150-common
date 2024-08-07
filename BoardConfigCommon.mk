e3ba50298b6bfaa16ddeef895d6c144b34bcc864
chematelegram <chfondia@gmail.com>
2024-08-07 14:16:43 +0200

    sm6150-common: Set flags for ELF files

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,8 @@ BOARD_VENDOR := xiaomi
 COMMON_PATH := device/xiaomi/sm6150-common
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
 
 # Inherit from proprietary files
 include vendor/xiaomi/sm6150-common/BoardConfigVendor.mk
